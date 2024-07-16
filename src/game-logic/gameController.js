d3ec6119a385d13e64c1628d8b594a96ac66fac8
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-07-16 15:40:39 +0200

    Create gameController class

---
+++ src/game-logic/gameController.js
@@ -0,0 +1,44 @@
+import { Player } from "./player";
+
+export class GameController {
+  constructor(playerOneName, playerOneType, playerTwoName, playerTwoType) {
+    this.players = [
+      new Player("Player One", "real"),
+      new Player("Player Two", "real"),
+    ];
+    this.activePlayer = this.players[0];
+    this.opponentPlayer = this.players[1];
+  }
+
+  getActivePlayer() {
+    return this.activePlayer;
+  }
+
+  getOpponentPlayer() {
+    return this.opponentPlayer;
+  }
+
+  switchPlayerTurn() {
+    if (this.activePlayer === this.players[0]) {
+      this.activePlayer = this.players[1];
+      this.opponentPlayer = this.players[0];
+    } else {
+      this.activePlayer = this.players[0];
+      this.opponentPlayer = this.players[1];
+    }
+  }
+
+  playRound(x, y) {
+    getOpponentPlayer().gameboard.receiveAttack(x, y);
+    if (!this.checkWinner()) this.switchPlayerTurn();
+  }
+
+  checkWinner() {
+    if (this.getOpponentPlayer().gameboard.checkAllShipsSunk()) return true;
+    return false;
+  }
+
+  getPlayerBoard() {
+    return this.activePlayer.gameboard.board;
+  }
+}
