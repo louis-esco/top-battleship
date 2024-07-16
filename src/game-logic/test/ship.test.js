eb0f90f85132842f257d9b3d2b4c80597585e415
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-07-16 15:40:39 +0200

    Remove useless code from test sequence

--- src/game-logic/test/ship.test.js
+++ src/game-logic/test/ship.test.js
@@ -1,29 +1,27 @@
 import { Ship } from "../ship";
 
-describe("Validator", () => {
-  test("Create new ship", () => {
-    expect(new Ship(3)).toEqual({ length: 3, hits: 0, sunk: false });
+test("Create new ship", () => {
+  expect(new Ship(3)).toEqual({ length: 3, hits: 0, sunk: false });
+});
+
+const carrier = new Ship(5);
+
+test("Hit a ship", () => {
+  expect(carrier.hit()).toEqual({ length: 5, hits: 1, sunk: false });
+});
+
+test("Sink a ship", () => {
+  expect(carrier.hit().hit().hit().hit()).toEqual({
+    length: 5,
+    hits: 5,
+    sunk: true,
   });
+});
 
-  const carrier = new Ship(5);
-
-  test("Hit a ship", () => {
-    expect(carrier.hit()).toEqual({ length: 5, hits: 1, sunk: false });
-  });
-
-  test("Sink a ship", () => {
-    expect(carrier.hit().hit().hit().hit()).toEqual({
-      length: 5,
-      hits: 5,
-      sunk: true,
-    });
-  });
-
-  test("Can't hit sunk ship", () => {
-    expect(carrier.hit()).toEqual({
-      length: 5,
-      hits: 5,
-      sunk: true,
-    });
+test("Can't hit sunk ship", () => {
+  expect(carrier.hit()).toEqual({
+    length: 5,
+    hits: 5,
+    sunk: true,
   });
 });
