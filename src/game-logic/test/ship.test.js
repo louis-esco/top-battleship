42b5cb09b6729b79bcb8ab8e45e4b360abd86b1a
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-07-16 15:40:39 +0200

    Update ship test to add name property

--- src/game-logic/test/ship.test.js
+++ src/game-logic/test/ship.test.js
@@ -4,14 +4,20 @@ test("Create new ship", () => {
   expect(new Ship(3)).toEqual({ length: 3, hits: 0, sunk: false });
 });
 
-const carrier = new Ship(5);
+const carrier = new Ship(5, "carrier");
 
 test("Hit a ship", () => {
-  expect(carrier.hit()).toEqual({ length: 5, hits: 1, sunk: false });
+  expect(carrier.hit()).toEqual({
+    name: "carrier",
+    length: 5,
+    hits: 1,
+    sunk: false,
+  });
 });
 
 test("Sink a ship", () => {
   expect(carrier.hit().hit().hit().hit()).toEqual({
+    name: "carrier",
     length: 5,
     hits: 5,
     sunk: true,
@@ -20,6 +26,7 @@ test("Sink a ship", () => {
 
 test("Can't hit sunk ship", () => {
   expect(carrier.hit()).toEqual({
+    name: "carrier",
     length: 5,
     hits: 5,
     sunk: true,
